ecefcb82617b0642a4a5b9aba48ebe60df2b4486
rdjones98 <rdjones98@yahoo.com>
2016-12-11 22:14:10 -0600

    Add files via upload

--- scripts/MathEngine.js
+++ scripts/MathEngine.js
@@ -71,7 +71,7 @@ MathEngine.preprocessLogAndTrig = function(anExpr, aVal)
 		}
 		var inP = anExpr.substring(idx+aVal.length,eIdx);
 		var res = this.doMath(inP);
-		if( res != null && res.toString().indexOf("Infinity") > -1)
+		if( res == null || res.toString().indexOf("Infinity") > -1)
 			return "NaN";
 		if(aVal == "ln(")
 			res = Math.log(res);
